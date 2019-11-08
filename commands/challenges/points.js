1bf6593378c9cbe2a9ef74ecc8cbe12764d7bdc3
LegendXYT <46520558+LegendXYT@users.noreply.github.com>
2019-11-08 20:48:34 +1100

    Update points.js

--- commands/challenges/points.js
+++ commands/challenges/points.js
@@ -11,16 +11,8 @@ module.exports = {
     example1: "points",
     example2: "points <@513571036688547871>",
     run: async (bot, message, args, prefix) => {
-
- let member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.guild.members.find(x => x.displayName == args[0]) || message.member
- var output = await eco.FetchBalance(member.id)
- if (output.balance < 50) return message.reply(emote.cross + member.displayName + ` didn't register yet \`${prefix}register\` to get started!`)
-              const embed = new RichEmbed()
-                  .setColor("#2964bd")
-                  .setDescription(`:gem:  **${member.displayName + " has" || 'You have'} **\`${output.balance}\` Points!`)
-                  .setAuthor(member.displayName, member.user.displayAvatarURL)
-              message.channel.send(embed)
-
+    
+        message.channel.send("link to challenge leaderboard: https://docs.google.com/spreadsheets/d/1SlVtzBB0jfQIczm2bL55tDj_OVJMbR1HZrkc-QmZOJ4/edit?usp=sharing"
 
         }
       }
